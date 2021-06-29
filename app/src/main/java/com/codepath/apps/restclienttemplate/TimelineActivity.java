b00000e5c90902805435f3f31976d510a3ff2895
Vaidehi Phirke <vaidehiphirke@fb.com>
2021-06-29 17:07:23 -0500

    added logout capability, changed text to full_text (from API)

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -62,5 +63,10 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.i(TAG,"onFailure", throwable);
             }
         });
+    }
+
+    public void onLogoutButton(View view){
+        client.clearAccessToken(); // forget who's logged in
+        finish(); // navigate backwards to Login screen
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -15,7 +15,7 @@ public class Tweet {
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
-        tweet.body = jsonObject.getString("text");
+        tweet.body = jsonObject.getString("full_text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
         return tweet;
