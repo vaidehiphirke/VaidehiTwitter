4bfa5fbaa5d62e9193417f7d4bf13e0bf5bd9625
Robert M. Vunabandi <23228006+robertvunabandi@users.noreply.github.com>
2021-06-30 17:23:13 -0400

    Update ComposeActivity.java
    example of reducing levels of indentation

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -51,41 +51,43 @@ public class ComposeActivity extends AppCompatActivity {
         composeButton = composeBinding.btnCompose;
 
         composeTextLayout.setCounterMaxLength(Tweet.MAX_TWEET_LENGTH);
-
+        // TODO: create ComposerViewOnClickListener
+        composeButton.setOnClickListener(new ComposerViewOnClickListener());
+        // TODO: remove below
         composeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String tweetText = composeText.getText().toString();
                 if (tweetText.length() == 0) {
                     Toast.makeText(ComposeActivity.this, getString(R.string.compose_error_empty), Toast.LENGTH_SHORT).show();
-                } else if (tweetText.length() > Tweet.MAX_TWEET_LENGTH) {
+                    return;
+                } 
+                if (tweetText.length() > Tweet.MAX_TWEET_LENGTH) {
                     Toast.makeText(ComposeActivity.this, getString(R.string.compose_error_long), Toast.LENGTH_SHORT).show();
-                } else {
-                    client.publishTweet(tweetText, new JsonHttpResponseHandler() {
-                        @Override
-                        public void onSuccess(int statusCode, Headers headers, JSON json) {
-                            Log.i(TAG, "onSuccess publishing tweet");
-                            try {
-                                Tweet tweet = Tweet.fromJson(json.jsonObject);
-                                Log.i(TAG, "Published tweet says "+ tweet.body);
-                                Intent intent = new Intent();
-                                intent.putExtra("tweet", Parcels.wrap(tweet));
-                                setResult(RESULT_OK,intent);
-                                finish();//todo this isn't closing the activity
-                            } catch (JSONException e) {
-                                Log.i(TAG, "Published tweet says " + e);
-                                e.printStackTrace();
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                            Log.i(TAG, "onFailure publishing tweet" + response, throwable);
-                        }
-
-
-                    });
+                    return;
                 }
+                client.publishTweet(tweetText, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "onSuccess publishing tweet");
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG, "Published tweet says "+ tweet.body);
+                            Intent intent = new Intent();
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            setResult(RESULT_OK,intent);
+                            finish();//todo this isn't closing the activity
+                        } catch (JSONException e) {
+                            Log.i(TAG, "Published tweet says " + e);
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.i(TAG, "onFailure publishing tweet" + response, throwable);
+                    }
+                });
                 //ComposeActivity.this.finish();
             }
         });
